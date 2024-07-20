82954baff9291a6337eb95555f1ae094feb40956
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2024-07-20 13:26:09 +0530

    completed but validation error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormArray';
+
+
+
+  AddDetails = new FormGroup({
+
+    //1- Decalare formarray type control
+    skills : new FormArray(
+      [
+      new FormControl('', [Validators.required]),
+      new FormControl('', [Validators.required]),
+      ]
+    )
+
+
+  });
+
+
+
+  //3 - Creating this function to return the controls to the html where we loop through it
+  getSkillsControls() : FormArray{
+    return this.AddDetails.get('skills') as FormArray;
+  }
+
+
+
+  //4- Add to the array new controls
+  addSkills(){
+    (<FormArray>this.AddDetails.get('skills')).push(new FormControl('', [Validators.required]))
+  }
+
+
+  //5- Remove that control with index
+  removeSkills(i : number){
+    (<FormArray>this.AddDetails.get('skills')).removeAt(i)
+  }
+
+
+  onSubmit() : void {
+    console.log(this.AddDetails.value);
+  }
 }
